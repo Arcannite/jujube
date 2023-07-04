1162514b7f6e898ace2b9e5f1d769a1c9eb9eec0
LAPTOP-7IAK15V4\thhui <th.hui2002@gmail.com>
2023-07-04 18:09:54 +0800

    added previously untracked files

---
+++ src/app/products/page.js
@@ -0,0 +1,22 @@
+export default function Page() {
+  return (
+    <div className="mt-10 flex items-center justify-center gap-x-6">
+      <h1>Hello, Next.js!</h1>
+      <a
+        href="/"
+        className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      >
+        back to main
+      </a>
+      <div>
+        <a
+          href="products/placeholder1"
+          className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          go to placeholder1
+        </a>
+      </div>
+      {/* https://nextjs.org/docs/app/building-your-application/routing/linking-and-navigating#linking-to-dynamic-segments */}
+    </div>
+  )
+}
